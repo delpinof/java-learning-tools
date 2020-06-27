37624f521b826eaf09301a602ff890f03ef973c4
Fernando del Pino <fdelpinoruiz@homeaway.com>
2020-06-27 02:04:12 -0500

    adding different scenarios for powers of 3

--- src/test/java/com/fherdelpino/test/challenge/PowerOf3Equals.java
+++ src/test/java/com/fherdelpino/test/challenge/PowerOf3Equals.java
@@ -1,5 +1,11 @@
 package com.fherdelpino.test.challenge;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -9,8 +15,8 @@ import lombok.extern.slf4j.Slf4j;
 public class PowerOf3Equals {
 
     private static final int MIN = 1;
-    private static final int MAX = 100;
-    private int count = 0;
+    private static final int MAX = 100; //usually 1K
+    private int count;
 
     @Before
     public void setUp() {
@@ -31,7 +37,6 @@ public class PowerOf3Equals {
                 }
             }
         }
-        log.info("total={}", count);
     }
 
     @Test
@@ -49,7 +54,6 @@ public class PowerOf3Equals {
                 }
             }
         }
-        log.info("total={}", count);
     }
 
     @Test
@@ -68,6 +72,68 @@ public class PowerOf3Equals {
                 }
             }
         }
+    }
+
+    @Test
+    public void pairs() {
+        final String separator = ":";
+        Map<Integer, List<String>> pairs = new HashMap<>();
+        for (int c = MIN; c <= MAX; c++) {
+            for (int d = MIN; d <= MAX; d++) {
+                int key = (int) (Math.pow(c, 3) + Math.pow(d, 3));
+                if (!pairs.containsKey(key)) {
+                    pairs.put(key, new ArrayList<>());
+                }
+                pairs.get(key).add(c + separator + d);
+            }
+        }
+
+        for (int a = MIN; a <= MAX; a++) {
+            for (int b = MIN; b <= MAX; b++) {
+                int result = (int) (Math.pow(a, 3) + Math.pow(b, 3));
+                for (String cdPair : pairs.get(result)) {
+                    String[] cdArray = cdPair.split(separator);
+                    int c = Integer.parseInt(cdArray[0]);
+                    int d = Integer.parseInt(cdArray[1]);
+                    log.info("a={}, b={}, c={}, d={}", a, b, c, d);
+                    count++;
+                }
+            }
+        }
+    }
+
+    @Test
+    public void pair() {
+        final String separator = ":";
+        Map<Integer, List<String>> pairs = new HashMap<>();
+        for (int c = MIN; c <= MAX; c++) {
+            for (int d = MIN; d <= MAX; d++) {
+                int key = (c * c * c) + (d * d * d);
+                if (!pairs.containsKey(key)) {
+                    pairs.put(key, new ArrayList<>());
+                }
+                pairs.get(key).add(c + separator + d);
+            }
+        }
+        for (List<String> results : pairs.values()) {
+            for (String pair1 : results) {
+                for (String pair2 : results) {
+                    String[] abArray = pair1.split(separator);
+                    String[] cdArray = pair2.split(separator);
+                    log.info("a={}, b={}, c={}, d={}",
+                        abArray[0],
+                        abArray[1],
+                        cdArray[0],
+                        cdArray[1]
+                    );
+                    count++;
+                }
+            }
+        }
+    }
+
+    @After
+    public void tearDown() {
         log.info("total={}", count);
     }
 }
