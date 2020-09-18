4b512a2c6e1e3168a4895f5c573e50a5ea371400
Fernando del Pino <fherdelpino@hotmail.com>
2020-09-18 03:48:45 -0500

    enhance binary search

--- src/test/java/com/fherdelpino/test/collections/search/BinarySearchTest.java
+++ src/test/java/com/fherdelpino/test/collections/search/BinarySearchTest.java
@@ -4,8 +4,8 @@ import com.fherdelpino.collections.search.BinarySearch;
 import com.fherdelpino.test.collections.CollectionsTestUtils;
 import org.junit.Test;
 
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+
 
 public class BinarySearchTest {
 
@@ -13,31 +13,31 @@ public class BinarySearchTest {
     public void searchIterative10NumbersFound() {
         int[] testData = CollectionsTestUtils.getTestDataNConsecutiveInts(10);
         BinarySearch binarySearch = new BinarySearch();
-        boolean result = binarySearch.searchIterative(testData, 9);
-        assertTrue(result);
+        int result = binarySearch.searchIterative(testData, 9);
+        assertTrue(result > 0);
     }
 
     @Test
     public void searchIterative10NumbersNotFound() {
         int[] testData = CollectionsTestUtils.getTestDataNConsecutiveInts(10);
         BinarySearch binarySearch = new BinarySearch();
-        boolean result = binarySearch.searchIterative(testData, 11);
-        assertFalse(result);
+        int result = binarySearch.searchIterative(testData, 11);
+        assertTrue(result < 0);
     }
 
     @Test
     public void searchRecursive10NumbersFound() {
         int[] testData = CollectionsTestUtils.getTestDataNConsecutiveInts(10);
         BinarySearch binarySearch = new BinarySearch();
-        boolean result = binarySearch.searchRecursive(testData, 9);
-        assertTrue(result);
+        int result = binarySearch.searchRecursive(testData, 9);
+        assertTrue(result > 0);
     }
 
     @Test
     public void searchRecursive10NumbersNotFound() {
         int[] testData = CollectionsTestUtils.getTestDataNConsecutiveInts(10);
         BinarySearch binarySearch = new BinarySearch();
-        boolean result = binarySearch.searchRecursive(testData, 11);
-        assertFalse(result);
+        int result = binarySearch.searchRecursive(testData, 11);
+        assertTrue(result < 0);
     }
 }
