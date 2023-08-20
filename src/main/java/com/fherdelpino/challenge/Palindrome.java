9442a7b56794ab0a3a00e8b64d17153cb4284191
Fernando del Pino <fherdelpino@hotmail.com>
2023-08-20 17:50:36 +1000

    add find palindrome challenge

--- src/main/java/com/fherdelpino/challenge/Palindrome.java
+++ src/main/java/com/fherdelpino/challenge/Palindrome.java
@@ -17,4 +17,30 @@ public class Palindrome {
             return string[left] == string[right] && isPalindrome(string, left + 1, right - 1);
         }
     }
+
+    /**
+     * Find the palindromic string in a String s
+     *
+     * @param s the string that has a palindrome in any place.
+     * @return the palindromic String
+     */
+    public static String findPalindrome(String s) {
+        String[] letters = s.split("");
+        int start = 0;
+        int end = 0;
+        for (int i = 0; i < letters.length - 2; i++) {
+            boolean pairPalindrome = letters[i].equals(letters[i + 1]);
+            boolean oddPalindrome = letters[i].equals(letters[i + 2]);
+            if (pairPalindrome || oddPalindrome) {
+                start = i - 1;
+                end = i + (pairPalindrome ? 2 : 3);
+                while (start >= 0 && end < letters.length && letters[start].equals(letters[end])) {
+                    start--;
+                    end++;
+                }
+                break;
+            }
+        }
+        return start == end ? "" : s.substring(start + 1, end);
+    }
 }
--- src/test/java/com/fherdelpino/challenge/PalindromeTest.java
+++ src/test/java/com/fherdelpino/challenge/PalindromeTest.java
@@ -1,6 +1,11 @@
 package com.fherdelpino.challenge;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -11,5 +16,22 @@ public class PalindromeTest {
         var palindrome = "Dabale arroz a la zorra el abad";
         var isPalindrome = Palindrome.isPalindrome(palindrome);
         assertThat(isPalindrome).isTrue();
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    public void testFindPalindrome(String input, String expectedOutput) {
+        String actualOutput = Palindrome.findPalindrome(input);
+        assertThat(actualOutput).isEqualTo(expectedOutput);
+    }
+
+    private static Stream<Arguments> testFindPalindrome() {
+        return Stream.of(
+                Arguments.of("abcdefgabccbahijklm", "abccba"),
+                Arguments.of("abcdefghijklmnopqzxyxz", "zxyxz"),
+                Arguments.of("abcdefghijklmnopq", ""),
+                Arguments.of("zxyxzabcdefghijklmnopq", "zxyxz"),
+                Arguments.of("abccbaabcdefghijklmnopq", "abccba")
+        );
     }
 }
