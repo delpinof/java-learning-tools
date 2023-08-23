9583588a899e15173530213e94ed33a85ba289ac
Fernando del Pino <fherdelpino@hotmail.com>
2023-08-24 00:59:53 +1000

    improve trie

--- src/main/java/com/fherdelpino/datastructures/Trie.java
+++ src/main/java/com/fherdelpino/datastructures/Trie.java
@@ -3,12 +3,12 @@ package com.fherdelpino.datastructures;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.function.Consumer;
 
 public class Trie {
 
-    private char c;
-    private Set<Trie> childs;
+    private final char c;
+    private final Set<Trie> childs;
 
     public Trie() {
         c = ' ';
@@ -21,19 +21,21 @@ public class Trie {
     }
 
     public void addWord(String word) {
-        char[] chars = word.toLowerCase().toCharArray();
-        char tmpC = chars[0];
-        List<Trie> tries = childs.stream()
-                .filter(trie -> trie.c == tmpC)
-                .collect(Collectors.toList());
-        if (tries.size() == 0) {
-            Trie t = new Trie(tmpC);
-            if (chars.length > 1)
-                t.addWord(word.substring(1));
-            childs.add(t);
-        } else {
-            tries.get(0).addWord(word.substring(1));
+        if (word.length() < 1) {
+            return;
         }
+        char tmpC = word.toCharArray()[0];
+
+        Consumer<Trie> addWordToChild = child -> child.addWord(word.substring(1));
+        Runnable addNewTrie = () -> {
+            Trie t = new Trie(tmpC);
+            t.addWord(word.substring(1));
+            childs.add(t);
+        };
+        childs.stream()
+                .filter(child -> child.c == tmpC)
+                .findFirst()
+                .ifPresentOrElse(addWordToChild, addNewTrie);
     }
 
     public boolean searchWord(String word) {
@@ -41,7 +43,7 @@ public class Trie {
         char tmpC = chars[0];
         List<Trie> tries = childs.stream()
                 .filter(trie -> trie.c == tmpC)
-                .collect(Collectors.toList());
+                .toList();
         if (tries.size() == 0) {
             return false;
         } else {
