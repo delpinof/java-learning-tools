54d29861b4b687fa26147f64467bf1299b23d938
Fernando del Pino <fherdelpino@hotmail.com>
2023-10-13 22:22:00 +1100

    add new mergeSort algorithm and test

--- src/main/java/com/fherdelpino/datastructures/collections/sort/MergeSort.java
+++ src/main/java/com/fherdelpino/datastructures/collections/sort/MergeSort.java
@@ -38,4 +38,48 @@ public class MergeSort {
 
         return numbers;
     }
+
+    public void mergeSort(int[] numbers) {
+        int size = numbers.length;
+        sort(numbers, new int[size], 0, size);
+    }
+
+    public void sort(int[] numbers, int[] tmp, int leftStart, int rightEnd) {
+        if (leftStart >= rightEnd) {
+            return;
+        }
+        int middle = (leftStart + rightEnd) / 2;
+        sort(numbers, tmp, leftStart, middle);
+        sort(numbers, tmp, middle + 1, rightEnd);
+        merge(numbers, tmp, leftStart, rightEnd);
+    }
+
+    private void merge(int[] array, int[] tmp, int leftStart, int rightEnd) {
+        int mid = (leftStart + rightEnd) / 2;
+
+        int tmpIndex = leftStart;
+        int leftIndex = leftStart;
+        int rightIndex = mid + 1;
+
+        while (leftIndex <= mid && rightIndex <= rightEnd) {
+            if (array[leftIndex] < array[rightIndex]) {
+                tmp[tmpIndex++] = array[leftIndex++];
+            } else {
+                tmp[tmpIndex++] = array[rightIndex++];
+            }
+        }
+
+        while (leftIndex <= mid) {
+            tmp[tmpIndex++] = array[leftIndex++];
+        }
+
+        while (rightIndex <= rightEnd) {
+            tmp[tmpIndex++] = array[rightIndex++];
+        }
+
+        while (--tmpIndex >= leftStart) {
+            array[tmpIndex] = tmp[tmpIndex];
+        }
+
+    }
 }
--- src/test/java/com/fherdelpino/datastructures/collections/sort/MergeSortTest.java
+++ src/test/java/com/fherdelpino/datastructures/collections/sort/MergeSortTest.java
@@ -2,8 +2,13 @@ package com.fherdelpino.datastructures.collections.sort;
 
 import com.fherdelpino.datastructures.collections.CollectionsTestUtils;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -11,47 +16,63 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @Slf4j
 public class MergeSortTest {
 
-    MergeSort mergeSort;
+    private static MergeSort sut;
 
-    @BeforeEach
-    public void setUp() {
-        mergeSort = new MergeSort();
+    @BeforeAll
+    public static void setUp() {
+        sut = new MergeSort();
     }
 
     @Test
     public void test4Ints() {
         int[] testData = {2, 4, 3, 1};
-        int[] result = mergeSort.sort(testData);
+        int[] result = sut.sort(testData);
         assertThat(result).isSorted();
     }
 
     @Test
     public void test3Ints() {
         int[] testData = {2, 3, 1};
-        int[] result = mergeSort.sort(testData);
+        int[] result = sut.sort(testData);
         assertThat(result).isSorted();
     }
 
     @Test
     public void test10Ints() {
         int[] testData = CollectionsTestUtils.testData10IntsReverseOrder.clone();
-        int[] result = mergeSort.sort(testData);
+        int[] result = sut.sort(testData);
         assertThat(result).isSorted();
     }
 
     @Test
     public void test1MInts() {
-        int[] testData = CollectionsTestUtils.getTestDataRandomNInts(1_000_001);
+        int[] testData = CollectionsTestUtils.getTestDataRandomNInts(1_000_001).clone();
         long timeStart = System.currentTimeMillis();
-        int[] result = mergeSort.sort(testData);
+        sut.sort(testData, new int[testData.length], 0, testData.length - 1);
         long timeTaken = System.currentTimeMillis() - timeStart;
         log.info("time={}", timeTaken);
-        assertThat(result).isSorted();
+        assertThat(testData).isSorted();
     }
 
     @Test
     public void intDivision() {
         int size = 3;
         assertEquals(size / 2, 1);
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    public void testMergeSort(int[] unOrderedArray) {
+        int[] result = unOrderedArray.clone();
+        sut.sort(result, new int[result.length], 0, result.length - 1);
+        assertThat(result).containsExactlyInAnyOrder(unOrderedArray);
+        assertThat(result).isSorted();
+    }
+
+    public static Stream<Arguments> testMergeSort() {
+        return Stream.of(
+                Arguments.of(new int[]{2, 3, 1}),
+                Arguments.of(new int[]{2, 4, 3, 1})
+        );
     }
 }
