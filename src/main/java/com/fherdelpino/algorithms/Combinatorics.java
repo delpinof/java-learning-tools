3b621917051ddb28f7c300b204445788b8291e25
Fernando del Pino <fherdelpino@hotmail.com>
2024-06-26 18:44:59 +1000

    add combinatorics

--- src/main/java/com/fherdelpino/algorithms/Combinatorics.java
+++ src/main/java/com/fherdelpino/algorithms/Combinatorics.java
@@ -1,8 +1,13 @@
 package com.fherdelpino.algorithms;
 
-import java.util.ArrayList;
-import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
 public class Combinatorics {
 
     public static List<List<String>> findCombinations(String[] values, int size) {
@@ -24,5 +29,63 @@ public class Combinatorics {
         List<String> tmp = new ArrayList<>(combination);
         tmp.add(values[index]);
         powerSet(values, index + 1, limit, result, tmp);
+    }
+
+    public static List<List<Integer>> getAllSubList(List<Integer> numbers) {
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < numbers.size(); i++) {
+            for (int j = i + 1; j <= numbers.size(); j++) {
+                List<Integer> subList = numbers.subList(i, j);
+                result.add(subList);
+            }
+        }
+        return result;
+    }
+
+    public static int sumList(List<Integer> numbers) {
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return sum;
+    }
+
+    public static List<Integer> getPrefixSum(List<Integer> numbers) {
+        List<Integer> prefixSum = new ArrayList<>();
+        for (int i = 0; i < numbers.size(); i++) {
+            if (i == 0) {
+                prefixSum.add(i, numbers.get(i));
+            } else {
+                int prefix = prefixSum.get(i - 1) + numbers.get(i);
+                prefixSum.add(i, prefix);
+            }
+        }
+        return prefixSum;
+    }
+
+    public static List<Integer> getAllSubListsSums1(List<Integer> numbers) {
+        List<List<Integer>> subLists = Combinatorics.getAllSubList(numbers);
+        Map<List<Integer>, Integer> sums = new HashMap<>();
+        for(List<Integer> subList : subLists) {
+            int sum = Combinatorics.sumList(subList);
+            sums.put(subList, sum);
+        }
+        return new ArrayList<>(sums.values()) ;
+    }
+
+    public static List<Integer> getAllSubListSums2(List<Integer> numbers) {
+        List<Integer> prefixSum = Combinatorics.getPrefixSum(numbers);
+        List<Integer> sums = new ArrayList<>();
+        for (int i = 0; i < numbers.size(); i++) {
+            for (int j = i; j < numbers.size(); j++) {
+                int sum = prefixSum.get(j);
+                int leftIndex = j - i - 1;
+                if (leftIndex >= 0) {
+                    sum-=prefixSum.get(leftIndex);
+                }
+                sums.add(sum);
+            }
+        }
+        return sums;
     }
 }
--- src/test/java/com/fherdelpino/algorithms/CombinatoricsTest.java
+++ src/test/java/com/fherdelpino/algorithms/CombinatoricsTest.java
@@ -1,11 +1,13 @@
 package com.fherdelpino.algorithms;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Slf4j
 public class CombinatoricsTest {
 
     @Test
@@ -15,5 +17,35 @@ public class CombinatoricsTest {
         assertThat(result).containsExactlyInAnyOrder(List.of("a", "b"),
                 List.of("a", "c"),
                 List.of("b", "c"));
+    }
+
+    @Test
+    public void testAllSubLists() {
+        List<Integer> numbers = List.of(1, 2, 3);
+        List<List<Integer>> result = Combinatorics.getAllSubList(numbers);
+        log.info("{}", result);
+    }
+
+    @Test
+    public void testSums() {
+        List<Integer> numbers = List.of(1, 2, 3, 4);
+        List<Integer> sums = Combinatorics.getAllSubListsSums1(numbers);
+        log.info("{}", sums);
+    }
+
+    @Test
+    public void testPrefixSum() {
+        List<Integer> numbers = List.of(1, 2, 3, 4);
+        List<Integer> prefixSum = Combinatorics.getPrefixSum(numbers);
+        log.info("{}", prefixSum);
+    }
+
+    @Test
+    public void testAllSumsWithPrefixSumAlgorithm() {
+        List<Integer> numbers = List.of(1, 2, 3);
+        List<Integer> sums1 = Combinatorics.getAllSubListsSums1(numbers);
+        List<Integer> sums2 = Combinatorics.getAllSubListSums2(numbers);
+        assertThat(sums2).containsAll(sums1);
+        log.info("{}", sums2);
     }
 }
