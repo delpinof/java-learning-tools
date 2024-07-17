0d858883283d4c0e59051144da6be8ccda80c90c
Fernando del Pino <fherdelpino@hotmail.com>
2024-07-17 02:51:26 -0600

    improve map test

--- src/test/java/com/fherdelpino/mapping/TestFunctionalMapping.java
+++ src/test/java/com/fherdelpino/mapping/TestFunctionalMapping.java
@@ -1,21 +1,25 @@
 package com.fherdelpino.mapping;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.assertj.core.api.Assertions.assertThat;
 
 
+@Slf4j
 public class TestFunctionalMapping {
 
-	@Test
-	public void test() {
-		Map<String,Integer> namesMap = new HashMap<>();
+    @Test
+    public void testComputeIfAbsent() {
+        Map<String, Integer> namesMap = new HashMap<>();
 
-		namesMap.computeIfAbsent("Fernando", String::length);
-		namesMap.computeIfAbsent("del Pino", String::length);
+        namesMap.computeIfAbsent("Fernando", String::length);
+        namesMap.computeIfAbsent("del Pino", String::length);
 
-		System.out.println(namesMap);
-	}
+        log.info("{}", namesMap);
+        assertThat(namesMap).contains(Map.entry("Fernando", 8), Map.entry("del Pino", 8));
+    }
 }
