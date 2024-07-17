df7896a7c7401909042f72f753192d772b73ba94
Fernando del Pino <fherdelpino@hotmail.com>
2024-07-17 02:46:55 -0600

    improve nondivisiblesubset challenge

--- src/main/java/com/fherdelpino/challenge/NonDivisibleSubset.java
+++ src/main/java/com/fherdelpino/challenge/NonDivisibleSubset.java
@@ -12,36 +12,22 @@ public class NonDivisibleSubset {
     Given a set of distinct integers, print the size of a maximal subset of  where the sum of any numbers in s is not evenly divisible by k.
      */
     public static int find(int k, List<Integer> s) {
-        List<Integer> numbers = new ArrayList<>();
-        int maxCount = 0;
-        for (int i = 0; i < s.size(); i++) {
-            numbers.add(s.get(i));
-            for (int j = 0; j < s.size(); j++) {
-                if (i == j) {
-                    continue;
-                }
-                if (isNotEvenlyDivisible(k, s.get(j), numbers)) {
-                    numbers.add(s.get(j));
-                }
-            }
-            //log.info("from {} - {}", i, numbers);
-            if (numbers.size() > maxCount) {
-                maxCount = numbers.size();
-            }
-
-            numbers = new ArrayList<>();
+        int[] remainer = new int[k];
+        for(int n : s){
+            remainer[n % k]++;
         }
 
-        return maxCount;
+        int res = (remainer[0] > 0) ? 1 : 0;
+
+        if(k % 2 == 0){
+            res += (remainer[k / 2] > 0) ? 1 : 0;
+        }
+
+        for(int i = 1; i <= (k-1)/2 ; i++){
+            res += Math.max(remainer[i], remainer[k-i]);
+        }
+
+        return res;
     }
 
-    private static boolean isNotEvenlyDivisible(int k, int j, List<Integer> numbers) {
-        for (int n : numbers) {
-            int sum = n + j;
-            if (sum % k == 0) {
-                return false;
-            }
-        }
-        return true;
-    }
 }
--- src/test/java/com/fherdelpino/challenge/NonDivisibleSubsetTest.java
+++ src/test/java/com/fherdelpino/challenge/NonDivisibleSubsetTest.java
@@ -1,11 +1,14 @@
 package com.fherdelpino.challenge;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -14,12 +17,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 @Slf4j
 public class NonDivisibleSubsetTest {
 
+    private static List<Integer> bigDataSet;
+
+    @BeforeAll
+    public static void load() throws IOException {
+        InputStream is = NonDivisibleSubsetTest.class.getClassLoader().getResourceAsStream("data/data.txt");
+        String textData = new String(is.readAllBytes()).replaceAll("\\s+$", "");
+        bigDataSet = Stream.of(textData.split(" ")).map(Integer::parseInt).toList();
+        log.info("{}", bigDataSet);
+    }
+
     @Test
     public void testLargeIntegers() {
         int s = Integer.MAX_VALUE;
         int n = Integer.MAX_VALUE;
 
-        int result = s+1;
+        int result = s + 1;
         log.info("{}", result);
     }
 
@@ -32,6 +45,9 @@ public class NonDivisibleSubsetTest {
 
     static Stream<Arguments> testNonDivisibleSubset() {
         return Stream.of(
+                Arguments.of(4,
+                        List.of(19,10,12,10,24,25,22),
+                        3),
                 Arguments.of(3,
                         List.of(1, 7, 2, 4),
                         3),
@@ -40,7 +56,8 @@ public class NonDivisibleSubsetTest {
                         11),
                 Arguments.of(5,
                         List.of(770528134, 663501748, 384261537, 800309024, 103668401, 538539662, 385488901, 101262949, 557792122, 46058493),
-                        6)
+                        6),
+                Arguments.of(50, bigDataSet, 47410)
         );
     }
 }
